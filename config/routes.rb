c27d2d43d0cc0f360f40fecead868c5427a3e250
Franquinet Aubry <aubry.franquinet@hotmail.com>
2022-01-18 13:54:57 +0100

    Added authentication tokens

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :items
   namespace :api do
     namespace :v1 do
       resources :products, only: [:index, :show, :create]
     end
   end
 
+   post 'authenticate', to: 'authentication#authenticate'
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_17_153157) do
+ActiveRecord::Schema.define(version: 2022_01_18_121153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -30,6 +37,7 @@ ActiveRecord::Schema.define(version: 2022_01_17_153157) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
 end
